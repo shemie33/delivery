258c30dc5d1915a697aacd42128749a66a9fa116
Tofoya Page <tofoya@gmail.com>
2020-02-02 03:11:38 -0500

    pushing a build trigger

--- main.py
+++ main.py
@@ -13,5 +13,11 @@ def name(value):
     val = {"value": value}
     return jsonify(val)
 
+
+@app.route('/Shem')
+def Shem():
+    val = {"value": "Shem"}
+    return jsonify(val)
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
